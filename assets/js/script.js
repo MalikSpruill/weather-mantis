abe6fc7791615e51754d3273ebe9617f785c517b
Malik <malikspruill@gmail.com>
2022-03-26 23:34:44 -0400

    Added functionality to enter a city and add the button under previous searches.  Logs weather data of city in console log.  Forces users to enter data in the search field

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,84 @@
 let APIKey = "4beb2f3c075f29c9148fcd6830d2242d";
-let cityName = "austin";
-let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKey}&units=imperial`;
-let pictureHolder = document.getElementById("weather-info");
-let iconHolder;
+//let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKey}&units=imperial`;
+let formEl = document.getElementById("search-field");
+let inputEl = document.getElementById("search-input");
+let searchesEl = document.getElementById("past-searches");
+let savedCities = [];
+
+//invokes with form submission - Searches city weather
+let showWeather = function (event) {
+    event.preventDefault();
+
+    let searchedCity = inputEl.value;
+    inputEl.value = "";
+    if (searchedCity === "") {
+       inputEl.setAttribute("placeholder", "Search an actual city!");
+       return;
+    }
+
+    // store cities that aren't yet saved
+    let cityStored = JSON.parse(localStorage.getItem("cities"));
+      if ( cityStored == null || !cityStored.includes(searchedCity)) {
+          let btnEl = document.createElement("button");
+          btnEl.textContent = searchedCity;
+          searchesEl.appendChild(btnEl);
+          savedCities.push(searchedCity);
+          localStorage.setItem("cities",JSON.stringify(savedCities));
+    }
+
+    //pass coordinates to access specific weather data from api
+    let coordinates = weatherCaster(searchedCity,APIKey);
+    coordinates.then(data => {
+        let latitude = data[0];
+        let longitude = data[1];
+        let queryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,hourly,alerts&units=imperial&appid=${APIKey}`
+        let officialWeatherData = fetch(queryURL)
+        .then(response => response.ok? response.json(): console.log("Error: On first response"))
+        .then( data => console.log(data))
+    })
+
+}
+
+let weatherCaster = function(searchedCity, apikey) {
+    let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&appid=${apikey}&units=imperial`;
+
+    let data = fetch(queryURL)
+    .then(response => response.ok ? response.json() : console.log("Error: On first response"))
+    .then(data => {
+        let latitude = data.coord.lat;
+        let longitude = data.coord.lon;
+        let latitudeLon = [];
+        latitudeLon.push(latitude, longitude);
+        return latitudeLon;
+    })
+    .catch(error => console.log(`${error}: Look further into weatherCaster function`))
+
+    return data;
+}
 
 
-//"https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
-//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-//let iconUrl = "http://openweathermap.org/img/wn/" + iconHolder + ".png";
-// Works for 5 day weather let queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + city + "&lon=" + city + "&exclude=minutely,hourly,alerts&units=imperial&appid=" + APIKey;
-//WORKS   let queryURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + APIKey;
-//fetch(`api.openweathermap.org/data/2.5/forecast?lat=-97.7431&lon=30.2672&appid=${APIKey}`)
 
 
-    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=raleigh&appid=${APIKey}`)
+
+
+formEl.addEventListener("submit", showWeather);
+
+//function that invokes on startup for existing saved cities
+
+
+//let iconUrl = "http://openweathermap.org/img/wn/" + iconVariable + ".png";
+//api for lat,lon   https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${APIKey}&units=imperial
+//api for 5 day https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=imperial&appid=${APIKey}
+
+
+
+/*
+    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${APIKey}&units=imperial`)
     .then(response => response.json())
     .then(data => {
+        console.log(data);
         let lat = data[0].lat;
-        let lon = data[0].lat;
+        let lon = data[0].lon;
         let arr = [];
         arr.push(lat,lon);
         console.log(arr);
@@ -26,26 +87,6 @@ let iconHolder;
         .then(response => response.json())
         .then(data => console.log(data))
      })
-
-
-fetch(queryURL)
-.then(response => response.json())
-.then(data => {
-    console.log(data);
-    /*
-    let lon = data.coord.lon;
-    let lat = data.coord.lat;
-    console.log(lon + "  " + lat);
-    iconHolder = data.weather[0].icon;
-    let iconUrl = "http://openweathermap.org/img/wn/" + iconHolder + ".png";
-    let image = document.createElement("img");
-    console.log(pictureHolder)
-
-    image.setAttribute("src", iconUrl);
-    pictureHolder.appendChild(image);
-    console.log(pictureHolder);
-    */
-})
-
+*/
 
 
