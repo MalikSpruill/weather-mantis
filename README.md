aff426887299468526772cdb2b015d5b657c9db0
Malik <malikspruill@gmail.com>
2022-03-27 14:54:23 -0400

    Configured Css styling for better user experience

--- README.md
+++ README.md
@@ -1,11 +1,27 @@
-# Make It Work
+# Weather Mantis
 
 ## Goal for this project
-A weather dashboard for users to search cities and view their forecasts.
+A weather dashboard for users to search cities and view their forecasts. Searched cities will also be saved, and you'll be able to revisit the page for further viewing.  
 
+### Current Forecast Stats:
+* **Temp**
+* **Wind**
+* **Humidity**
+* **UV Index**
+
+### Weekly Forecast Stats:
+* **Temp**
+* **Wind**
+* **Humidity**
+
+<img src="./assets/images/weather-mantis-s.png" width="500px" />
+
+<br />
 
 ## Technologies Used
-
+JavaScript
+CSS
+date-fns
 
 ## Contributions
 Malik Spruill
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 let APIKey = "4beb2f3c075f29c9148fcd6830d2242d";
-//let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKey}&units=imperial`;
 let formEl = document.getElementById("search-field");
 let inputEl = document.getElementById("search-input");
 let searchesEl = document.getElementById("past-searches");
@@ -10,14 +9,11 @@ let savedCities = [];
 
 //invokes with form submission - Searches city weather
 let showWeather = function (event) {
-    
     if (event.type == "submit") {
         event.preventDefault();
     }
-    
     cityDateEl.innerHTML = "";
     extendedForecastEl.innerHTML = "";
-    //document.querySelector("#extended-forecast h2").style.display = "none";
 
     let searchedCity = inputEl.value;
     inputEl.value = "";
@@ -87,7 +83,7 @@ let showWeather = function (event) {
                 cardWind.textContent = `Wind: ${data.daily[weatherCard].wind_speed} MPH`;
                 cardHumidity.textContent = `Humditiy: ${data.daily[weatherCard].humidity} %`;
 
-                weatherDivEl.append(cardDate,cardIcon,cardTemp,cardWind);
+                weatherDivEl.append(cardDate,cardIcon,cardTemp,cardWind, cardHumidity);
                 extendedForecastEl.append(weatherDivEl);
             }
             console.log(data)
@@ -112,7 +108,6 @@ let weatherCaster = function(searchedCity, apikey) {
 
     return data;
 }
-
 
 let uvColor = function(uv) {
     switch(uv) {
@@ -140,7 +135,6 @@ let previouslySearched = function() {
     }
 }
 
-
  let chosenPreviousCity = function(event) {
     console.log(event.type);
     let targetEl = event.target;
@@ -148,37 +142,7 @@ let previouslySearched = function() {
     showWeather(event);
     } 
 
-
-
 document.addEventListener("DOMContentLoaded", previouslySearched);
 searchesEl.addEventListener("click", chosenPreviousCity);
 formEl.addEventListener("submit", showWeather);
-//searchesEl.addEventListener("click",previouslySearched);
-
-//function that invokes on startup for existing saved cities
-
-
-//let iconUrl = "http://openweathermap.org/img/wn/" + iconVariable + ".png";
-//api for lat,lon   https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${APIKey}&units=imperial
-//api for 5 day https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=imperial&appid=${APIKey}
-
-
-
-/*
-    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${APIKey}&units=imperial`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        let lat = data[0].lat;
-        let lon = data[0].lon;
-        let arr = [];
-        arr.push(lat,lon);
-        console.log(arr);
-
-        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=imperial&appid=${APIKey}`)
-        .then(response => response.json())
-        .then(data => console.log(data))
-     })
-*/
-
 
